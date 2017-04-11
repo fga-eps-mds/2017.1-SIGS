1b33cce24ef80d800665fbfa45eb1828aacaa215
mendesiasmin <mendesiasmin96@gmail.com>
2017-04-11 13:09:48 -0300

    Create model Course

---
+++ SIGS/db/migrate/20170411160430_create_courses.rb
@@ -0,0 +1,10 @@
+class CreateCourses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :courses do |t|
+      t.string :code
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
