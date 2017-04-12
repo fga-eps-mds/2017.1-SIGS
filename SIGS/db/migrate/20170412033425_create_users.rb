24ef8fa34797ae428ce220068005b9805dd1d4b7
mendesiasmin <mendesiasmin96@gmail.com>
2017-04-12 00:35:50 -0300

    Create User Model

---
+++ SIGS/db/migrate/20170412033425_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :cpf
+      t.string :registration
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
