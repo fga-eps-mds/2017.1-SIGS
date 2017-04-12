b722a8f3a6d3a9558ef701075bed4a86a03aec3d
Wallacy Braz <wallacy.braz@live.com>
2017-04-12 19:51:06 -0300

    Create administrative assistant controller

---
+++ SIGS/app/controllers/administrative_assistant_controller.rb
@@ -0,0 +1,37 @@
+class AdministrativeAssistantController < ApplicationController
+  def registration_request
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def remove
+  end
+
+  def enable
+  end
+
+  def approve_registration
+  end
+
+  def approve_allocation
+  end
+
+  def index_users
+  end
+
+  def view_users
+  end
+
+  def edit_users
+  end
+
+  def remove_users
+  end
+end
---
+++ SIGS/app/helpers/administrative_assistant_helper.rb
@@ -0,0 +1,2 @@
+module AdministrativeAssistantHelper
+end
