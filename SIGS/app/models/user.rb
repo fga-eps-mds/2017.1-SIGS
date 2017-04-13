40e5b8e8f7556ca0f95d1e9f6be4f34ed27160a1
Bruno Matias <leguenta@hotmail.com>
2017-04-12 22:55:15 -0300

    Add column password_digest to controller users

--- SIGS/app/models/user.rb
+++ SIGS/app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_one :coordinator, dependent: :destroy
   has_one :administrative_assistant, dependent: :destroy
   has_one :department_assistant, dependent: :destroy
+  has_secure_password
 end
--- SIGS/db/schema.rb
+++ SIGS/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412131327) do
+ActiveRecord::Schema.define(version: 20170413014301) do
 
   create_table "administrative_assistants", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(version: 20170412131327) do
     t.string   "cpf"
     t.string   "registration"
     t.boolean  "active"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_foreign_key "administrative_assistants", "users"
