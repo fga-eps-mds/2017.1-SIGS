c91a8cf455c01626d4abce53b977b0c7e3dbe6ff
Bruno Matias <leguenta@hotmail.com>
2017-04-13 00:07:52 -0300

    Implement methods in helper of sessions

--- SIGS/app/controllers/sessions_controller.rb
+++ SIGS/app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
-    render "new"
   end
 
   def create
     user = User.find_by(email: params[:session][:email])
       if user && user.authenticate(params[:session][:password])
         sign_in(user)
+        redirect_to current_user
+      else
+        render 'new'
+      end
   end
 
   def destroy
--- SIGS/app/helpers/sessions_helper.rb
+++ SIGS/app/helpers/sessions_helper.rb
@@ -2,4 +2,18 @@ module SessionsHelper
   def sign_in(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def block_access
+    if current_user.present?
+      redirect_to current_user
+    end
+  end
+
+  def logged_in?
+      !current_user.nil?
+  end
 end
