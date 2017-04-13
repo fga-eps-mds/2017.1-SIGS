0fdabe72bb3adc48878bba72c4deeba32a78c63b
Carlos Aragon <carlosaragon3@hotmail.com>
2017-04-12 21:37:23 -0300

    Update user controller

---
+++ SIGS/app/controllers/user_controller.rb
@@ -0,0 +1,32 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  #Creating a new user
+  def create
+  	@user = User.new(user_params)
+    if @user.save
+    end
+  end
+
+  # Editing the user profile
+  def edit
+    if @user != current_user
+    end
+  end
+
+  #Update User
+  def update
+    if @user.update(user_params)
+      flash[:success] = t(:sucessful_profile_update)
+      redirect_to @user
+    else
+      flash[:warning] = t(:error_profile_update)
+    end
+  end
+
+  private
+  def user_params
+    params[:user].permit(:name, :email, :password, :registration, :cpf, :active)
+end
