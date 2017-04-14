6b2348fc682081d0719e3fc6ef1c160429a0b1be
Daniel Marques <danielmarques7@hotmail.com>
2017-04-14 16:41:44 -0300

    Add commits of methods

--- SIGS/app/controllers/department_assistant_controller.rb
+++ SIGS/app/controllers/department_assistant_controller.rb
@@ -1,26 +1,25 @@
 class DepartmentAssistantController < ApplicationController
 
-
-  
-
+  # Método de página de perfil do Assistente de Departmento
   def index
     @department_assistant = DepartmentAssistant.find(params[:id])
     @user = User.find(@department_assistant.user_id)
   end
 
+  # Método de visualizar perfil do Assistente de Departmento
   def show
     @department_assistant = DepartmentAssistant.find(params[:id])
     @user = User.find(@department_assistant.user_id)
     @department = Department.find(@department_assistant.department_id)
   end
 
+  # Método de visualizar conta do Assistente de Departmento
   def destroy
     @department_assistant = DepartmentAssistant.find(params[:id])
     @user = User.find(@department_assistant.user_id)
 
     @department_assistants = DepartmentAssistant.all
     if @department_assistants.count > 1
-      @department_assistant.destroy
       @user.destroy
       redirect_to destroy_path
     else
@@ -29,6 +28,8 @@ class DepartmentAssistantController < ApplicationController
   end
 
   private
+  # Método de parametros do Assistente de Departmento
   def department_assistant_params
     params[:department_assistant].permit(:user_id,:department_id)
+  end
 end
