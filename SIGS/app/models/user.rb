e0ed24d073a3749b52bf3e48d88b5a981a9c9338
Carlos Aragon <carlosaragon3@hotmail.com>
2017-04-16 18:52:27 -0300

    Update validations

--- SIGS/app/models/user.rb
+++ SIGS/app/models/user.rb
@@ -10,29 +10,27 @@ class User < ApplicationRecord
 validates_length_of :name,
 		:within => 7..100,
 		:too_short => 'Nome tem que ter no minimo 7 caracters',
-		:too_long => 'Nome tem que ter no maximo 100 caracters'
+		:too_long => 'Nome pode ter no maximo 100 caracters'
 
 # email
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-validates :email, :presence => { :message => 'Email nao pode ser vazio' },
+validates :email, :presence => { :message => 'Email não pode ser vazio' },
 	length: { maximum: 50}, uniqueness: true, confirmation: true,
 	format: { with: VALID_EMAIL_REGEX, :message => 'Insira um e-mail válido'}
 
 # password
-validates :password, length: { minimum: 6, maximum: 20, :message => 'Senha deve possuir no mínimo 4 e máximo de 16 caracteres' }, confirmation: true, on: :create		  			
+validates :password, length: { minimum: 6, maximum: 20, :message => 'Senha deve possuir o mínimo 4 e máximo de 16 caracteres' }, confirmation: true, on: :create		  			
 
 # cpf
 VALID_CPF_REGEX = /\A[0-9]{3}?[0-9]{3}?[0-9]{3}?[0-9]{2}\z/i
 
 validates :cpf, :presence => { :message => 'Cpf nao pode ser vazio' },
-	length: { maximum: 11}, uniqueness: true,
+	length: { is: 11}, uniqueness: true,
 	format: { with: VALID_CPF_REGEX, :message => 'Insira um Cpf válido'}
 
 # registration
-# validates_length_of :origin,
-# 		:within => 9
-# 		:too_short => 'Matricula deve possuir 9 digitos',
-# 		:too_long => 'Matricula deve possuir 9 digitos'
+validates :registration, :presence => { :message => 'Matricula nao pode ser vazio' },
+	length: { is: 9}, uniqueness: true
 
 end
