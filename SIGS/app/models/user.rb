a6a18b9758c57b75c9ba3b9541329fe8d838924f
Carlos Aragon <carlosaragon3@hotmail.com>
2017-04-17 20:57:42 -0300

    Update email and registration validations

--- SIGS/app/models/user.rb
+++ SIGS/app/models/user.rb
@@ -15,7 +15,7 @@ validates_length_of :name,
 		:too_long => 'Nome pode ter no maximo 100 caracters'
 
 # email
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@unb.br\z/i
 
 validates :email, :presence => { :message => 'Email não pode ser vazio' },
 	length: { maximum: 50}, uniqueness: true, confirmation: true,
@@ -30,9 +30,9 @@ VALID_CPF_REGEX = /\A[0-9]{3}?[0-9]{3}?[0-9]{3}?[0-9]{2}\z/i
 validates :cpf, :presence => { :message => 'Cpf nao pode ser vazio' },
 	length: { is: 11}, uniqueness: true,
 	format: { with: VALID_CPF_REGEX, :message => 'Insira um Cpf válido'}
-
+	
 # registration
 validates :registration, :presence => { :message => 'Matricula nao pode ser vazio' },
-	length: { is: 9}, uniqueness: true
+	length: { is: 7}, uniqueness: true
 
 end
