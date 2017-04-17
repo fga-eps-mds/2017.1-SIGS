383b751a1cd30b71f4f1824801bc57ba6e441991
Daniel Marques <danielmarques7@hotmail.com>
2017-04-17 20:38:51 -0300

    Implement method registration request, enable and decline of Administrative Assistant Controller

--- SIGS/config/routes.rb
+++ SIGS/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 ### Sessions - Routes
   get 'sign_in' => 'sessions#new'
   post 'sign_in' => 'sessions#create'
-  get 'sign_out' => 'sessions#destroy'
+  delete 'sign_out' => 'sessions#destroy'
 ###
 
 ### Coordinator - ROUTES
@@ -45,6 +45,12 @@ Rails.application.routes.draw do
   post 'administrative_assistant/update_users/:id' => 'administrative_assistant#update_users' , as:'update_users'
 
   get 'administrative_assistant/destroy_users/:id' => 'administrative_assistant#destroy_users' , as: 'destroy_users'
+
+  get 'administrative_assistant/registration_request' => 'administrative_assistant#registration_request', as: 'registration_request'
+
+  get 'administrative_assistant/enable_registration/:id' => 'administrative_assistant#enable_registration', as: 'enable_registration'
+
+  get 'administrative_assistant/decline_registration/:id' => 'administrative_assistant#decline_registration', as: 'decline_registration'
 ###
 
 ### Parsers
