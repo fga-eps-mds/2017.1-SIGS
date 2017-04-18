19a037ff9a6b89e83bc17cd7a40b86f41ecf584f
Daniel Marques <danielmarques7@hotmail.com>
2017-04-17 23:21:34 -0300

    Refactor of seeds.rb

--- SIGS/db/seeds.rb
+++ SIGS/db/seeds.rb
@@ -6,57 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# ======= Departament =========
-department_1 = Department.new
-department_1.code = '789'
-department_1.name = 'Engenharia'
-department_1.save!
+# Departaments
+department = Department.create(code: '789', name: 'Engenharia')
 
-# ========= Courses ===============
-course_1 = Course.new
-course_1.code = '10'
-course_1.name = 'Engenharia de Software'
-course_1.department_id = department_1.id
-course_1.save!
+# Courses
+course = Course.create(code: '10', name: 'Engenharia de Software', department: department)
 
-# ========== User - Coordinator ==============
-user_1 = User.new
-user_1.name = 'Caio Filipe'
-user_1.email = 'caio@unb.br'
-user_1.password = '123456'
-user_1.cpf = '05012345678'
-user_1.registration = '1234567'
-user_1.save!
+# Users - Coordinator
+user_1 = User.create(name: 'Caio Filipe', email: 'caio@unb.br', cpf: '05012345678', registration: '1234567', active: true, password: '123456')
+coordinator = Coordinator.create(user: user_1, course: course, department_id: department.id)
 
-coordinator_1 = Coordinator.new
-coordinator_1.user_id = user_1.id
-coordinator_1.course_id = course_1.id
-coordinator_1.department_id = department_1.id
-coordinator_1.save!
+# Users - DepartmentAssistant
+user_2 = User.create(name: 'João Pedro', email: 'joao@unb.br', cpf: '05012349999', registration: '1234599', active: true, password: '123456')
+department_assistant = DepartmentAssistant.create(user: user_2, department: department)
 
-# ========== User - DepartmentAssistant =================
-user_1 = User.new
-user_1.name = 'João Pedro'
-user_1.email = 'joao@unb.br'
-user_1.password = '123456'
-user_1.cpf = '05012349999'
-user_1.registration = '1234599'
-user_1.save!
-
-department_assistant_1 = DepartmentAssistant.new
-department_assistant_1.user_id = user_1.id
-department_assistant_1.department_id = department_1.id
-department_assistant_1.save!
-
-# ========== User - AdministrativeAssistant =================
-user_1 = User.new
-user_1.name = 'Wallacy Braz'
-user_1.email = 'wallacy@unb.br'
-user_1.password = '123456'
-user_1.cpf = '05012348888'
-user_1.registration = '1234588'
-user_1.save!
-
-administrative_assistant_1 = AdministrativeAssistant.new
-administrative_assistant_1.user_id = user_1.id
-administrative_assistant_1.save!
+# Users - AdministrativeAssistant
+user_3 = User.create(name: 'Wallacy Braz', email: 'wallacy@unb.br', cpf: '05012348888', registration: '1234588', active: true, password: '123456')
+administrative_assistant = AdministrativeAssistant.create(user: user_3)
