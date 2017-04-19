e3917dae8f40ac229f4894f67a37cb837550b10a
Carlos Aragon <carlosaragon3@hotmail.com>
2017-04-19 02:34:34 -0300

    Update user models

--- SIGS/app/models/user.rb
+++ SIGS/app/models/user.rb
@@ -32,46 +32,6 @@ validates :cpf, :presence => { :message => 'Cpf nao pode ser vazio' },
 	:uniqueness => {:message => "Cpf já cadastrado no sistema"}
 
 # registration
-VALID_REGISTRATION_REGEX = /\A[0-9]{9}\z/i
-
-validates :registration, :presence => { :message => 'Matricula nao pode ser vazio' },
-	format: { with: VALID_REGISTRATION_REGEX, :message => 'Insira uma matricula válida'},
-	:uniqueness => {:message => "Matricula já cadastrado no sistema"}
-
-endclass User < ApplicationRecord
-  has_one :coordinator, dependent: :destroy
-  has_one :administrative_assistant, dependent: :destroy
-  has_one :department_assistant, dependent: :destroy
-  has_secure_password
-  accepts_nested_attributes_for :department_assistant, :reject_if => :all_blank
-  accepts_nested_attributes_for :coordinator, :reject_if => :all_blank
-  accepts_nested_attributes_for :administrative_assistant
-
-
-#name
-validates_length_of :name,
-		:within => 7..100,
-		:too_short => 'O Nome tem deve ter no minimo 7 caracters',
-		:too_long => 'Nome deve ter no maximo 100 caracters'
-
-# email
-VALID_EMAIL_REGEX = /\A[\w+\-.]+@unb.br\z/i
-
-validates :email, :presence => { :message => 'Email não pode ser vazio' },
-	length: { maximum: 50}, :uniqueness => { :message => 'Email já cadastrado no sistema' } ,
-	format: { with: VALID_EMAIL_REGEX, :message => 'Insira um e-mail válido'}
-
-# password
-validates :password, length: { minimum: 6, maximum: 20, :message => 'Senha deve possuir o mínimo 6 e máximo de 20 caracteres' }, confirmation: true, on: :create
-
-# cpf
-VALID_CPF_REGEX = /\A[0-9]{3}?[0-9]{3}?[0-9]{3}?[0-9]{2}\z/i
-
-validates :cpf, :presence => { :message => 'Cpf nao pode ser vazio' },
-	format: { with: VALID_CPF_REGEX, :message => 'Insira um Cpf válido'},
-	:uniqueness => {:message => "Cpf já cadastrado no sistema"}
-
-# registration
 VALID_REGISTRATION_REGEX = /\A[0-9]{7}\z/i
 
 validates :registration, :presence => { :message => 'Matricula nao pode ser vazio' },
