84d709d69cf84e813367fe84198b6afc4f94c0d0
Vinicius Pinheiro da Silva Correa <viny-pinheiro@hotmail.com>
2017-04-27 15:17:03 -0300

    Revert "Ts07 duplicated code refactoring model and helper"

--- SIGS/db/migrate/20170412034816_create_coordinators.rb
+++ SIGS/db/migrate/20170412034816_create_coordinators.rb
@@ -1,8 +1,10 @@
 class CreateCoordinators < ActiveRecord::Migration[5.0]
   def change
     create_table :coordinators do |t|
+      t.references :department, foreign_key: true
       t.references :course, foreign_key: true
       t.references :user, foreign_key: true
+
       t.timestamps
     end
   end
