eb4da3cb10f64922b7c1d5ac5219f648b23e34fb
WallacyBraz <wallacy.braz@live.com>
2017-04-27 14:58:29 -0300

    Appling tecprog in user model

--- SIGS/app/models/user.rb
+++ SIGS/app/models/user.rb
@@ -9,35 +9,49 @@ class User < ApplicationRecord
 
 
 #name
+CHARACTERS_MINIMUM_FOR_THE_NAME_EXCEPTION = 'O Nome deve ter no mínimo 7 caracteres'
+CHARACTERS_MAXIMUM_FOR_THE_NAME_EXCEPTION = 'Nome deve ter no máximo 100 caracters'
+
 validates_length_of :name,
 		:within => 7..100,
-		:too_short => 'O Nome deve ter no mínimo 7 caracteres',
-		:too_long => 'Nome deve ter no máximo 100 caracters'
+		:too_short => CHARACTERS_MINIMUM_FOR_THE_NAME_EXCEPTION,
+		:too_long => CHARACTERS_MAXIMUM_FOR_THE_NAME_EXCEPTION
 
 # email
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@unb.br\z/i
+EMAIL_EXISTS = 'E-mail já cadastrado no sistema'
+NULL_EMAIL = 'E-mail não pode ser vazio'
+INVALID_EMAIL = 'Insira um E-mail válido'
 
-validates :email, :presence => { :message => 'E-mail não pode ser vazio' },
-	length: { maximum: 50}, :uniqueness => { :message => 'E-mail já cadastrado no sistema' } ,
-	format: { with: VALID_EMAIL_REGEX, :message => 'Insira um E-mail válido'}
+validates :email, :presence => { :message => NULL_EMAIL },
+	length: { maximum: 50}, :uniqueness => { :message => EMAIL_EXISTS } ,
+	format: { with: VALID_EMAIL_REGEX, :message => INVALID_EMAIL}
 
 # password
-validates :password, length: { minimum: 6, maximum: 20, :message => 'Senha deve possuir no mínimo 6 e no máximo de 20 caracteres' }, confirmation: true, on: :create
+INVALID_LENGHT_PASSWORD = 'Senha deve possuir no mínimo 6 e no máximo de 20 caracteres'
+
+validates :password, length: { minimum: 6, maximum: 20, :message => INVALID_LENGHT_PASSWORD }, confirmation: true, on: :create
 
 
 # cpf
 VALID_CPF_REGEX = /\A[0-9]{3}?[0-9]{3}?[0-9]{3}?[0-9]{2}\z/i
+CPF_EXISTS = 'Cpf já cadastrado no sistema'
+NULL_CPF = 'Cpf nao pode ser vazio'
+INVALID_CPF = 'Insira um Cpf válido'
 
-validates :cpf, :presence => { :message => 'Cpf nao pode ser vazio' },
-  :uniqueness => {:message => "Cpf já cadastrado no sistema"},
-	format: { with: '/\A[0-9]{3}?[0-9]{3}?[0-9]{3}?[0-9]{2}\z/i', :message => 'Insira um Cpf válido'}
+validates :cpf, :presence => { :message => NULL_CPF },
+  :uniqueness => {:message => CPF_EXISTS },
+	format: { with: VALID_CPF_REGEX, :message => INVALID_CPF }
 
 # registration
 VALID_REGISTRATION_REGEX = /\A[0-9]{7}\z/i
+REGISTRATION_EXISTS = 'Matricula já cadastrado no sistema'
+NULL_REGISTRATION = 'Matricula nao pode ser vazio'
+INVALID_REGISTRATION = 'Insira uma matricula válida'
 
-validates :registration, :presence => { :message => 'Matricula nao pode ser vazio' },
-	format: { with: '/\A[0-9]{7}\z/i', :message => 'Insira uma matricula válida'},
-	:uniqueness => {:message => "Matricula já cadastrado no sistema"}
+validates :registration, :presence => { :message => NULL_REGISTRATION },
+	format: { with: VALID_REGISTRATION_REGEX, :message => INVALID_REGISTRATION},
+	:uniqueness => {:message => REGISTRATION_EXISTS }
 
 
 end
