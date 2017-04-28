7a2e46df56a1a13c05e6226a0c8a26c875c1984e
Ateldy Brasil <ateldybfilho@gmail.com>
2017-04-28 18:39:47 -0300

    Add seeds.rb in acceptance tests

--- SIGS/features/support/env.rb
+++ SIGS/features/support/env.rb
@@ -6,6 +6,8 @@
 
 require 'cucumber/rails'
 
+require File.dirname(__FILE__) + '/seeds'
+
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
