22e5d5846687eccf654dfe1302b4e9f593b6418e
Ateldy Brasil <ateldybfilho@gmail.com>
2017-04-28 18:56:34 -0300

    Change e-mail and put single quotation mark in feature and steps of keep_session

--- SIGS/features/step_definitions/keep_session_steps.rb
+++ SIGS/features/step_definitions/keep_session_steps.rb
@@ -2,26 +2,26 @@ Given (/^I am on the log in page$/) do
 	visit 'http://192.168.2.15:3000'
 end
 
-And (/^I fill in "email" with "caio@unb.br"$/) do
-	fill_in('session[email]', :with=> 'caio@unb.br')
+And (/^I fill in 'email' with 'wallacy@unb.br'$/) do
+	fill_in('session[email]', :with=> 'wallacy@unb.br')
 end
 
-And (/^I fill in "email" with "invalidbot"$/) do
+And (/^I fill in 'email' with 'invalidbot'$/) do
 	fill_in('session[email]', :with=> 'invalidbot')
 end
 
-And (/^I fill in "password" with "123456"$/) do
+And (/^I fill in 'password' with '123456'$/) do
 	fill_in('session[password]', :with=> '123456')
 end
 
-When (/^I press "Entrar" button$/) do
-	click_button("Entrar")
+When (/^I press 'Entrar' button$/) do
+	click_button('Entrar')
 end
 
-Then (/^the initial page should load with notice message "Login realizado com sucesso"$/) do
-	expect(page).to have_content("Email ou senha incorretos")
+Then (/^the initial page should load with notice message 'Login realizado com sucesso'$/) do
+	expect(page).to have_content('Login realizado com sucesso')
 end
 
-Then (/^the login page should reload with notice message "Login ou senha inválidos"$/) do
-	expect(page).to have_content("Email ou senha incorretos")
+Then (/^the login page should reload with notice message 'Login ou senha inválidos'$/) do
+	expect(page).to have_content('Email ou senha incorretos')
 end
