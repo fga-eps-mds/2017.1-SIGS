5b6d888e5787bcb20923b9ef5335c25f5ea5638d
mendesiasmin <mendesiasmin96@gmail.com>
2017-05-06 10:17:57 -0300

    Update school_room model and Add migrations

--- SIGS/app/models/school_room.rb
+++ SIGS/app/models/school_room.rb
@@ -1,4 +1,4 @@
 class SchoolRoom < ApplicationRecord
   belongs_to :discipline
-  has_and_belongs_to_many :course, uniq: true
+  has_and_belongs_to_many :course
 end
--- SIGS/db/migrate/20170504173708_create_courses_school_rooms_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateCoursesSchoolRoomsJoinTable < ActiveRecord::Migration[5.0]
-  def change
-    create_table :courses_school_rooms, id: false do |table|
-      table.references :course, foreign_key: true
-      table.references :school_room, foreign_key: true
-    end
-  end
-end
