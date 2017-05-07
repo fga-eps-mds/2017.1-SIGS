7867a6a602a67696d0355d2c8d6f926a10e99ffb
DanielMarques28 <danielmarques7@hotmail.com>
2017-05-06 22:57:55 -0300

    Fix tests failed in room spec

--- SIGS/spec/controllers/rooms_controller_spec.rb
+++ SIGS/spec/controllers/rooms_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe RoomsController, type: :controller do
   describe 'Rooms controller methods' do
     before(:each) do
       @building = Building.create(code: 'ICC', name: 'ICC', wing: 'norte')
-      
+
       @room = Room.create(code: 'S10', name: 'Superior 10', capacity: 50,
        active: true, time_grid_id: 1, building_id: @building.id)
 
       @user = User.create(name: 'joao silva', email: 'joaosilva@unb.br',
         password: '123456', registration:'1100061', cpf:'05601407380', active: true)
-    end 
-    
+    end
+
     it 'should return all room' do
       post :index
       expect(response).to have_http_status(200)
@@ -40,7 +40,7 @@ RSpec.describe RoomsController, type: :controller do
     it 'should not update room because code is nil' do
       sign_in(@user)
       get :update, params:{id: @room.id, room: {name: 'Novo Nome', code: ''}}
-      expect(flash.now[:warning]).to eq('Dados não foram atualizados')
+      expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
 
     it 'should not update room because code exists' do
@@ -48,31 +48,31 @@ RSpec.describe RoomsController, type: :controller do
       Room.create(code: 'S110', name: 'Superior 10', capacity: 50,
        active: true, time_grid_id: 1, building_id: @building.id)
       get :update, params:{id: @room.id, room: {code: 'S110'}}
-      expect(flash.now[:warning]).to eq('Dados não foram atualizados')
+      expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
 
     it 'should not update room because name has one character' do
       sign_in(@user)
       get :update, params:{id: @room.id, room: {name: 'S'}}
-      expect(flash.now[:warning]).to eq('Dados não foram atualizados')
+      expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
 
     it 'should not update room because name is long' do
       sign_in(@user)
       get :update, params:{id: @room.id, room: {name: '012345678901234567890123456789012345678901234567890'}}
-      expect(flash.now[:warning]).to eq('Dados não foram atualizados')
+      expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
 
     it 'should not update room because capacity is less than 5' do
       sign_in(@user)
       get :update, params:{id: @room.id, room: {capacity: 2}}
-      expect(flash.now[:warning]).to eq('Dados não foram atualizados')
+      expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
 
     it 'should not update room because capacity is more than 500' do
       sign_in(@user)
       get :update, params:{id: @room.id, room: {capacity: 800}}
-      expect(flash.now[:warning]).to eq('Dados não foram atualizados')
+      expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
 
   end
