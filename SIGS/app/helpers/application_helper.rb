159ac81964555ca71ad09d9087a231f16bb2c508
Carlos Aragon <carloseraragon@gmail.com>
2017-05-09 10:20:26 -0300

    issue #27: refactoring helpers

--- SIGS/app/helpers/application_helper.rb
+++ SIGS/app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
+# module to show messages
+class ParsersController < ApplicationController
 module ApplicationHelper
   def flash_message
-   messages = ""
-   [:notice, :info, :warning, :error].each {|type|
-     if flash[type]
-       messages += "#{flash[type]}"
-     end
-   }
-
-   messages
- end
+	  messages = ''
+	  [:notice, :info, :warning, :error].each { |type|
+		  if flash[type]
+		  	messages += "#{flash[type]}"
+		  end
+			else
+			end
+	  }
+  	messages
+	end
 end
--- SIGS/app/helpers/coordinator_helper.rb
+++ SIGS/app/helpers/coordinator_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# coordinator module
 module CoordinatorHelper
 end
