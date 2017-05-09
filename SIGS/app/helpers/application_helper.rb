d168d6f6532b8382cdfb4bc8fd36db7c20327f6b
matias0422 <leguenta@hotmail.com>
2017-05-09 16:02:16 -0300

    issue #27: Refactor models parser, coordinator and helper application

--- SIGS/app/helpers/application_helper.rb
+++ SIGS/app/helpers/application_helper.rb
@@ -4,11 +4,9 @@
 module ApplicationHelper
   def flash_message
     messages = ''
-    [:notice, :info, :warning, :error].each { |type|
-      if flash[type]
-        messages += flash[type].to_s
-      end
-    }
+    [:notice, :info, :warning, :error].each do |type|
+      messages += flash[type].to_s
+    end
     messages
   end
 end
