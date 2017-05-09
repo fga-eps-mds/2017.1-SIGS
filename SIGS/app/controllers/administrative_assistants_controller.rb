af72a946c09a799d31cd84d79761e148b01cc4b2
Carlos Aragon <carloseraragon@gmail.com>
2017-05-09 10:21:29 -0300

    issue #27: recfactoring controllers

--- SIGS/app/controllers/administrative_assistants_controller.rb
+++ SIGS/app/controllers/administrative_assistants_controller.rb
@@ -1,17 +1,18 @@
 class AdministrativeAssistantsController < ApplicationController
-
   before_action :logged_in?
   before_action :authenticate_administrative_assistant?
 
   def registration_request
     @users = User.where(active: false)
-    return @users
+    @users
   end
 
   def enable_registration
     @user = User.find(params[:id])
     if @user.update_attributes(active: true)
-      flash[:success] = "Usuário aprovado com sucesso"
+      flash[:success] = 'Usuário aprovado com sucesso'
+    end
+    else
     end
     redirect_to registration_request_path
   end
@@ -19,7 +20,7 @@ class AdministrativeAssistantsController < ApplicationController
   def decline_registration
     @user = User.find(params[:id])
     if @user.destroy
-      flash[:success] = "Usuário recusado com sucesso"
+      flash[:success] = 'Usuário recusado com sucesso'
     end
     redirect_to registration_request_path
   end
@@ -27,7 +28,7 @@ class AdministrativeAssistantsController < ApplicationController
   def destroy_users
     @user = User.find(params[:id])
     if @user.destroy
-      redirect_to user_index_path, :flash => {:sucess => 'Usuário excluído com sucesso'}
+      redirect_to user_index_path, flash: { sucess: 'Usuário excluído com sucesso' }
     end
   end
 end
