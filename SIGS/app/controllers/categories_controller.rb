b50109dd64ce5e0040fe4c558ab42e3313df1995
Matias0422 <leguenta@hotmail.com>
2017-05-16 19:50:23 -0300

    issue #30: Implement methods new and create of categories controller

--- SIGS/app/controllers/categories_controller.rb
+++ SIGS/app/controllers/categories_controller.rb
@@ -2,4 +2,16 @@
 
 # Controller Categories class
 class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(categories_params)
+    @category.save
+  end
+
+  def categories_params
+    params[:category].permit(:name, room_ids: [])
+  end
 end
