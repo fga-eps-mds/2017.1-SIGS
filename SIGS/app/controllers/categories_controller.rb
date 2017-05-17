d8a237bbacbd6347bed0f81d0470725b6d00ffa4
Ateldy Brasil <ateldybfilho@gmail.com>
2017-05-16 21:37:17 -0300

    issue #30: Implement method index and view of categories

--- SIGS/app/controllers/categories_controller.rb
+++ SIGS/app/controllers/categories_controller.rb
@@ -9,10 +9,15 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(categories_params)
     @category.save
+    redirect_to categories_index_path
     flash[:success] = 'Categoria criada'
   end
 
   def categories_params
-    params[:category].permit(:name, room_ids: [])
+    params[:category].permit(:name)
+  end
+
+  def index
+    @categories = Category.all
   end
 end
