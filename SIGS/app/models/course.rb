c45694106b7ecd5202bca0e6a6bbeff3edbab22b
Carlos Aragon <carloseraragon@gmail.com>
2017-05-23 13:33:48 -0300

    issue #19:Fix class relations

--- SIGS/app/models/course.rb
+++ SIGS/app/models/course.rb
@@ -5,4 +5,5 @@ class Course < ApplicationRecord
   has_and_belongs_to_many :disciplines
   has_one :coordinator, dependent: :destroy
   has_and_belongs_to_many :school_room
+  belongs_to :department
 end
--- SIGS/app/models/department.rb
+++ SIGS/app/models/department.rb
@@ -5,4 +5,6 @@ class Department < ApplicationRecord
   has_one :department_assistant, dependent: :destroy
   has_many :disciplines, dependent: :destroy
   has_many :rooms, dependent: :destroy
+  has_many :courses
+  has_many :coordinators
 end
