9c369115ef3f30153bb58bcb9a2529df8e9ea8f6
Carlos Aragon <carloseraragon@gmail.com>
2017-05-23 13:33:05 -0300

    issue #19:Add tests

--- SIGS/spec/controllers/rooms_controller_spec.rb
+++ SIGS/spec/controllers/rooms_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include SessionsHelper
 
 RSpec.describe RoomsController, type: :controller do
 
@@ -8,13 +9,25 @@ RSpec.describe RoomsController, type: :controller do
 
       @department = Department.create(code: '789', name: 'Engenharia')
 
+      @department_2 = Department.create(code: '749', name: 'PRC')
+
+      @course = Course.create(name: 'Musica', department: @department)
+
       @room = Room.create(code: 'S10', name: 'Superior 10', capacity: 50,
        active: true, time_grid_id: 1, building: @building, department: @department)
+
+      @room_2 = Room.create(code: 'S9', name: 'Superior 9', capacity: 50,
+       active: true, time_grid_id: 1, building: @building, department: @department_2)
 
       @user = User.create(name: 'joao silva', email: 'joaosilva@unb.br',
         password: '123456', registration:'1100061', cpf:'05601407380', active: true)
 
+      @user_2 = User.create(name: 'joao silva', email: 'joaferrera@unb.br',
+        password: '123456', registration:'1100069', cpf:'04601407380', active: true)
+
       @administrative_assistant = AdministrativeAssistant.create(user: @user)
+
+      @coordinator = Coordinator.create(user: @user_2, course: @course)
 
       sign_in(@user)
     end
@@ -44,7 +57,6 @@ RSpec.describe RoomsController, type: :controller do
       get :update, params:{id: @room.id, room: {name: 'Novo Nome', code: ''}}
       expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
-
     it 'should not update room because code exists' do
       Room.create(code: 'S110', name: 'Superior 10', capacity: 50,
        active: true, time_grid_id: 1, department: @department, building: @building)
@@ -71,6 +83,25 @@ RSpec.describe RoomsController, type: :controller do
       get :update, params:{id: @room.id, room: {capacity: 800}}
       expect(flash.now[:error]).to eq('Dados não foram atualizados')
     end
+
+    it 'should delete the room from the database, by coordinator' do 
+      get :destroy, params:{id: @room_2.id}
+      expect(flash[:success]).to eq('Sala excluida com sucesso')
+      expect(response).to redirect_to(room_index_path)
+    end
+
+    it 'should delete the room from the database, by administrative assistant' do
+    sign_in(@user_2) 
+      get :destroy, params:{id: @room.id}
+      expect(flash[:success]).to eq('Sala excluida com sucesso')
+      expect(response).to redirect_to(room_index_path)
+    end
+
+    it 'should not delete the room from the databasebecause the room belongs to another department' do
+      get :destroy, params:{id: @room.id}
+      expect(flash[:error]).to eq('Não possui permissão para excluir sala')
+      expect(response).to redirect_to(room_index_path)
+    end 
 
   end
 end
