b4798211f0b9914c7d8bf48f6dac529352670873
lucasandrad <lucasandradeunb@gmail.com>
2017-05-25 14:28:26 -0300

    issue #35 add method to show courses at school room index

--- SIGS/app/controllers/application_controller.rb
+++ SIGS/app/controllers/application_controller.rb
@@ -3,4 +3,5 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   include SessionsHelper
   include UserHelper
+  include SchoolRoomsHelper
 end
--- SIGS/app/helpers/school_rooms_helper.rb
+++ SIGS/app/helpers/school_rooms_helper.rb
@@ -1,5 +1,17 @@
-# frozen_string_literal: true
-
-# school room module
 module SchoolRoomsHelper
+
+  def courses_of_school_room(course_ids)
+    courses_names = []
+    if course_ids.kind_of?(Array)
+      course_ids.each do |course_id|
+        course = Course.find(course_id)
+        courses_names << course.name  
+      end
+    else 
+      course = Course.find(course_ids)
+      courses_names << course.name  
+    end
+    return courses_names
+  end
+
 end
