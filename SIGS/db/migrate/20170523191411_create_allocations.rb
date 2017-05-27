ed1b4a86ec6e15fe45a564c68ae70a73a82e53b7
VinyPinheiro <viny-pinheiro@hotmail.com>
2017-05-26 23:11:56 -0300

    issue #24: update attributes of allocation

--- SIGS/db/migrate/20170523191411_create_allocations.rb
+++ SIGS/db/migrate/20170523191411_create_allocations.rb
@@ -4,9 +4,11 @@ class CreateAllocations < ActiveRecord::Migration[5.0]
       t.boolean :active
       t.time :start_time
       t.time :final_time
-      t.string :day
+      t.date :day
+      t.integer :periodicity, default: 0 # 0 => semanal 1 => mensal
       t.references :user, foreign_key: true
       t.references :room, foreign_key: true
+      t.references :allocation_type, foreign_key: true
 
       t.timestamps
     end
