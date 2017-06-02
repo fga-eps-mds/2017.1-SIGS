6aa4a9489d04ac147ab77034cc35d19aa90075e3
mendesiasmin <mendesiasmin96@gmail.com>
2017-06-02 10:34:47 -0300

    Adapting the create method to receive multiple allocations

--- SIGS/app/controllers/allocations_controller.rb
+++ SIGS/app/controllers/allocations_controller.rb
@@ -7,6 +7,10 @@ class AllocationsController < ApplicationController
 
 
   def new
+    @allocations = []
+    3.times do
+      @allocations << Allocation.new
+    end
     @allocation = Allocation.new
     @coordinator_rooms = current_user.coordinator.course.department.rooms
     @school_rooms_coordinator = current_user.coordinator.course.school_rooms
@@ -14,29 +18,33 @@ class AllocationsController < ApplicationController
 
 
   def create
-    @allocation = Allocation.new(allocation_params)
-    @allocation.user_id = current_user.id
-    @allocations_of_day_and_room = Allocation.where(day: @allocation.day ,room_id: @allocation.room_id)
-    @no_save = 0
-    if time_invalid @allocation
-      flash[:error] = 'Horário inválido'
-    else
-      @allocations_of_day_and_room.each do |allocation_day_room|
-        if ((@allocation.start_time.strftime('%H').to_i >= allocation_day_room.start_time.strftime('%H').to_i &&
-            @allocation.start_time.strftime('%H').to_i <= allocation_day_room.final_time.strftime('%H').to_i) ||
-            (@allocation.final_time.strftime('%H').to_i >= allocation_day_room.start_time.strftime('%H').to_i &&
-            @allocation.final_time.strftime('%H').to_i <= allocation_day_room.final_time.strftime('%H').to_i))
-            @no_save = 1
-        end
-      end
-      if @no_save == 1 && same_schedule_and_diferent_school_room(@allocation) == false
-        flash[:error] = 'Alocação com horário não vago ou capacidade da sala cheia'
+
+    params["allocations"].each do |allocation|
+      @allocation = Allocation.new(allocations_params(allocation))
+      @allocation.user_id = current_user.id
+      @allocations_of_day_and_room = Allocation.where(day: @allocation.day ,room_id: @allocation.room_id)
+      @allocation.active = true
+      @no_save = 0
+
+      if time_invalid @allocation
+        flash[:error] = 'Horário inválido'
       else
-        if @allocation.save
-          pass_to_all_allocation_dates @allocation
-          flash[:success] = 'Alocação feita com sucesso'
+        @allocations_of_day_and_room.each do |allocation_day_room|
+          if ((@allocation.start_time.strftime('%H').to_i >= allocation_day_room.start_time.strftime('%H').to_i &&
+              @allocation.start_time.strftime('%H').to_i <= allocation_day_room.final_time.strftime('%H').to_i) ||
+              (@allocation.final_time.strftime('%H').to_i >= allocation_day_room.start_time.strftime('%H').to_i &&
+              @allocation.final_time.strftime('%H').to_i <= allocation_day_room.final_time.strftime('%H').to_i))
+              @no_save = 1
+          end
+        end
+        if @no_save == 1 && same_schedule_and_diferent_school_room(@allocation) == false
+          flash[:error] = 'Alocação com horário não vago ou capacidade da sala cheia'
         else
-          flash[:error] = 'Falha ao realizar alocação'
+          if @allocation.save
+            flash[:success] = 'Alocação feita com sucesso'
+          else
+            flash[:error] = 'Falha ao realizar alocação'
+          end
         end
       end
     end
@@ -128,5 +136,13 @@ class AllocationsController < ApplicationController
                                :day,
                                :start_time,
                                :final_time)
+  end
+
+  def allocations_params(my_params)
+    my_params.permit(:room_id,
+                     :school_room_id,
+                     :day,
+                     :start_time,
+                     :final_time)
   end
 end
