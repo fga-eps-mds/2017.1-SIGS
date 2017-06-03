797485938f745a086f0ad7a84373e80510744e64
danielmarques28 <danielmarques7@hotmail.com>
2017-06-03 14:59:20 -0300

    issue #24: Rebase development into us01_realize_allocation and organize of seeds.rb

--- SIGS/spec/controllers/allocations_controller_spec.rb
+++ SIGS/spec/controllers/allocations_controller_spec.rb
@@ -12,11 +12,11 @@ RSpec.describe AllocationsController, type: :controller do
       @user = User.create(name: 'Caio Filipe', email: 'caio@unb.br', cpf: '05012345678', registration: '1234567', active: true, password: '123456')
       @coordinator = Coordinator.create(user_id: @user.id,course_id:@course.id)
       @room = Room.create(code: '124325', name: 'S10', capacity: 50, active: true, time_grid_id: 1, department: @department, building: @building, category_ids: [@category.id])
-      @school_room = SchoolRoom.create(name:'A', active:true, discipline: @discipline, vacancies: 40, course_ids: [@course.id])
-      @school_room2 = SchoolRoom.create(name:'B', active:true, discipline: @discipline, vacancies: 40, course_ids: [@course.id])
-      @school_room3 = SchoolRoom.create(name:'C', active:true, discipline: @discipline2, vacancies: 40, course_ids: [@course.id])
-      @school_room4 = SchoolRoom.create(name:'D', active:true, discipline: @discipline, vacancies: 10, course_ids: [@course.id])
-      @school_room5 = SchoolRoom.create(name:'E', active:true, discipline: @discipline, vacancies: 20, course_ids: [@course.id])
+      @school_room = SchoolRoom.create(name:'A', discipline: @discipline, vacancies: 40, course_ids: [@course.id])
+      @school_room2 = SchoolRoom.create(name:'B', discipline: @discipline, vacancies: 40, course_ids: [@course.id])
+      @school_room3 = SchoolRoom.create(name:'C', discipline: @discipline2, vacancies: 40, course_ids: [@course.id])
+      @school_room4 = SchoolRoom.create(name:'D', discipline: @discipline, vacancies: 10, course_ids: [@course.id])
+      @school_room5 = SchoolRoom.create(name:'E', discipline: @discipline, vacancies: 20, course_ids: [@course.id])
       @period = Period.create(period_type:'Letivo', initial_date: '08-03-2018', final_date: '14-07-2018')
     end
 
