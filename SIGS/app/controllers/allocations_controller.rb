e069cdf8abf39518c2916bed940bd5a49ed01041
VinyPinheiro <viny-pinheiro@hotmail.com>
2017-06-15 00:02:25 -0300

    issue #52: Minimizing allocation_controller class stylesheet problems

--- SIGS/app/controllers/allocations_controller.rb
+++ SIGS/app/controllers/allocations_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable ClassLength
 # class that create allocations
 class AllocationsController < ApplicationController
   before_action :logged_in?
@@ -15,7 +16,9 @@ class AllocationsController < ApplicationController
     @coordinator_rooms = current_user.coordinator.course.department.rooms
   end
 
-
+  # rubocop:disable Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+  # rubocop:disable Metrics/LineLength
+  # rubocop:disable Style/WordArray, Style/MultilineArrayBraceLayout
   def create
     group_allocation = []
     valid = []
@@ -64,25 +67,23 @@ class AllocationsController < ApplicationController
 
   def save_allocation(allocation)
     new_allocation = Allocation.new(allocations_params(allocation))
-    if new_allocation.active
-      new_allocation.user_id = current_user.id
+    return unless new_allocation.active
+    new_allocation.user_id = current_user.id
 
-      if time_invalid (new_allocation)
-        flash[:error] = 'Horário inválido'
-      else
-        if verify_time_shock_room_day(new_allocation) && !super_allocation(new_allocation)
-          flash[:error] = 'Alocação com horário não vago ou capacidade da sala cheia'
-        else
-          if new_allocation.save
-            pass_to_all_allocation_dates(new_allocation)
-            flash[:success] = 'Alocação feita com sucesso'
-          else
-            flash[:error] = 'Falha ao realizar alocação'
-          end
-        end
-      end
+    if time_invalid(new_allocation)
+      flash[:error] = 'Horário inválido'
+    elsif verify_time_shock_room_day(new_allocation) && !super_allocation(new_allocation)
+      flash[:error] = 'Alocação com horário não vago ou capacidade da sala cheia'
+    elsif new_allocation.save
+      pass_to_all_allocation_dates(new_allocation)
+      flash[:success] = 'Alocação feita com sucesso'
+    else
+      flash[:error] = 'Falha ao realizar alocação'
     end
   end
+  # rubocop:enable Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+  # rubocop:enable Metrics/LineLength
+  # rubocop:enable Style/WordArray, Style/MultilineArrayBraceLayout
 
   def destroy
     @allocation_all_date_user = []
@@ -94,7 +95,7 @@ class AllocationsController < ApplicationController
   end
 
   def destroy_all_allocations
-    school_room_id= SchoolRoom.find(params[:id]).id
+    school_room_id = SchoolRoom.find(params[:id]).id
     allocations_sh = Allocation.where(school_room_id: school_room_id)
     allocations_sh.each(&:destroy)
     flash[:success] = 'Desalocação feita com sucesso'
@@ -123,79 +124,51 @@ class AllocationsController < ApplicationController
 
   private
 
-  def time_invalid (allocation)
+  def time_invalid(allocation)
     range = final_time_to_int(allocation) - start_time_to_int(allocation)
-
-    if (range < 1) || (range > 3) || invalid_shift(allocation)
-      return true
-    end
-    false
+    (range < 1) || (range > 3) || invalid_shift(allocation)
   end
 
-  def super_allocation (allocation)
+  def super_allocation(allocation)
     allocations_room = Allocation.where(room_id: allocation.room_id,
-    day: allocation.day,
-    start_time: allocation.start_time,
-    final_time: allocation.final_time)
+                                        day: allocation.day,
+                                        start_time: allocation.start_time,
+                                        final_time: allocation.final_time)
 
-    if verify_equals_disciplines(allocation, allocations_room)
-      return true
-    end
-    false
+    verify_equals_disciplines(allocation, allocations_room)
   end
 
   def verify_equals_disciplines(allocation, allocations_room)
     discipline = allocation.school_room.discipline
     vacancies = allocation.school_room.vacancies
     allocations_room.each do |allocation_room|
-      if discipline == allocation_room.school_room.discipline
-        if  allocation.school_room != allocation_room.school_room
-          vacancies += allocation_room.school_room.vacancies
-        else
-          return false
-        end
-      else
-        return false
-      end
+      return false unless discipline == allocation_room.school_room.discipline
+      return false unless allocation.school_room != allocation_room.school_room
+
+      vacancies += allocation_room.school_room.vacancies
     end
     vacancies <= allocation.room.capacity
   end
 
-def pass_to_all_allocation_dates allocation
-  period = Period.find_by(period_type: 'Letivo')
-  date = period.initial_date
-  while date != period.final_date do
-    all_allocation_date = AllAllocationDate.new
-    all_allocation_date.allocation_id = allocation.id
-    if allocation.day == "Segunda" && date.wday == 1
-      all_allocation_date.day = date
-      all_allocation_date.save
-      all_allocation_date = nil
-    elsif allocation.day == "Terça" && date.wday == 2
-      all_allocation_date.day = date
-      all_allocation_date.save
-      all_allocation_date = nil
-    elsif allocation.day == "Quarta" && date.wday == 3
-      all_allocation_date.day = date
-      all_allocation_date.save
-      all_allocation_date = nil
-    elsif allocation.day == "Quinta" && date.wday == 4
-      all_allocation_date.day = date
-      all_allocation_date.save
-      all_allocation_date = nil
-    elsif allocation.day == "Sexta" && date.wday == 5
-      all_allocation_date.day = date
-      all_allocation_date.save
-      all_allocation_date = nil
-    elsif allocation.day == "Sábado" && date.wday == 6
-      all_allocation_date.day = date
-      all_allocation_date.save
-      all_allocation_date = nil
-    end
-    date = date + 1
-  end
-end
+  def pass_to_all_allocation_dates(allocation)
+    period = Period.find_by(period_type: 'Letivo')
+    date = period.initial_date
+    while date != period.final_date
+      all_allocation_date = AllAllocationDate.new
+      all_allocation_date.allocation_id = allocation.id
 
+      %w[Segunda Terça Quarta Quinta Sexta Sabado].each_with_index do |day, index|
+        next unless allocation.day == day && date.wday == index + 1
+        all_allocation_date.day = date
+        all_allocation_date.save
+        all_allocation_date = nil
+      end
+      date += 1
+    end
+  end
+
+  # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  # rubocop:disable Metrics/LineLength
   def allocations_params(my_params)
     my_params.permit(:room_id,
                      :school_room_id,
@@ -206,17 +179,17 @@ end
   end
 
   def make_rows(hour)
-    @row = [(hour).to_s + ':00']
+    @row = [hour.to_s + ':00']
     %w[Segunda Terça Quarta Quinta Sexta Sabado].each do |day_of_week|
       allocations = Allocation.where(room_id: params[day_of_week]).where(day: day_of_week)
       allocations_start = allocations.where('DATE_FORMAT(start_time, "%H") <= ?', hour)
-                                .where('DATE_FORMAT(final_time, "%H") > ?', hour)
-      make_cell(allocations_start, hour, allocations, Room.find(params[day_of_week].to_i))
+                                     .where('DATE_FORMAT(final_time, "%H") > ?', hour)
+      make_cell(allocations_start, hour, Room.find(params[day_of_week].to_i))
     end
     @row
   end
 
-  def make_cell(allocations_start, hour, allocations, room)
+  def make_cell(allocations_start, hour, room)
     cell = ''
     if allocations_start.size.zero?
       cell = ' '
@@ -225,61 +198,60 @@ end
       exist = false
       allocations_start.each do |allocation|
         cell += allocation.school_room.discipline.name unless exist
-        cell += "<br>Turma:" +
+        cell += '<br>Turma:' +
                 allocation.school_room.name
         exist = true
       end
     end
-    data_allocation =  []
+    data_allocation = []
     data_allocation.push cell
     data_allocation.push params[:school_room]
     data_allocation.push room.id
     data_allocation.push hour.to_s
-    data_allocation.push (hour + 1).to_s
+    data_allocation.push((hour + 1).to_s)
     data_allocation.push SchoolRoom.find(params[:school_room]).courses.first.shift
     @row << data_allocation
-
   end
 
-  def start_time_to_int (allocation)
+  def start_time_to_int(allocation)
     allocation.start_time.strftime('%H').to_i
   end
 
-  def final_time_to_int (allocation)
+  def final_time_to_int(allocation)
     allocation.final_time.strftime('%H').to_i
   end
 
-  def time_in_range? (hour, allocation)
+  def time_in_range?(hour, allocation)
     start_interval = start_time_to_int(allocation)
     final_interval = final_time_to_int(allocation)
     hour >= start_interval && hour <= final_interval
   end
 
-  def verify_time_shock (new_allocation, allocation_room)
+  def verify_time_shock(new_allocation, allocation_room)
     start = start_time_to_int(new_allocation)
     final = final_time_to_int(new_allocation)
     time_in_range?(start, allocation_room) || time_in_range?(final, allocation_room)
   end
 
-  def verify_time_shock_room_day (allocation)
-    allocations_room = Allocation.where(day: allocation.day ,room_id: allocation.room_id)
+  def verify_time_shock_room_day(allocation)
+    allocations_room = Allocation.where(day: allocation.day, room_id: allocation.room_id)
     allocations_room.each do |allocation_room|
-      if verify_time_shock(allocation, allocation_room)
-        return true
-      end
+      return true if verify_time_shock(allocation, allocation_room)
     end
     false
   end
 
-  def get_allocation_shift (allocation)
+  def get_allocation_shift(allocation)
     allocation.school_room.courses.first.shift
   end
 
-  def invalid_shift (allocation)
+  def invalid_shift(allocation)
     shift = get_allocation_shift(allocation)
     final = final_time_to_int(allocation)
     start = start_time_to_int(allocation)
     (shift == 1 && final > 18) || (shift == 2 && start < 18)
   end
-
+  # rubocop:enable Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/LineLength
 end
+# rubocop:enable ClassLength
