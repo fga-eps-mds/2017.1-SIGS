aa0ab358b10a3b019361f187304173076b5127cf
Rdadamos <rodrigodadamos@gmail.com>
2017-06-18 13:26:15 -0300

    issue #74: prototype done
    Signed-off-by: Rdadamos <rodrigodadamos@gmail.com>
    Signed-off-by: Wallacy Braz <wallacy.braz@live.com>

--- SIGS_prototype/assets/js/menu.js
+++ SIGS_prototype/assets/js/menu.js
@@ -1,32 +1,22 @@
-$(document).ready(function (){
-  // addItem("Minha Conta", "minha_conta", "myNavBar");
-  // addItem("Logout", "../index", "myNavBar");
-  addItem("Alocar", "visualizar_turmas", "myNavBar");
-  addItem("Cadastros Pendentes", "cadastros_pendentes", "Usuários");
-  addItem("Usuários Registrados", "usuarios_registrados", "Usuários");
-  // menuDropdown("Turmas");
+$(document).ready(function () {
+  addItem("Alocar", "visualizar_turmas", "myNav", "col2");
+  menuDropdown("Usuários");
+  addItem("Cadastros Pendentes", "cadastros_pendentes", "Usuários", "col1");
+  addItem("Usuários Registrados", "usuarios_registrados", "Usuários", "col1");
   menuDropdown("Salas");
   menuDropdown("Relatórios");
-  addItem("Salas", "salas", "Salas");
-  addItem("Categorias", "categorias", "Salas");
-  // menuDropdown("Alocar");
-  // addItem("Nova Turma", "criar_turma", "Alocar");
-  // addItem("Meu Departamento", "visualizar_turmas", "Alocar");
-  // addItem("Graduação", "home", "Alocar")
-  // addItem("Extensão", "home", "Alocar")
-  // addItem("Solicitar(alocacao)", "solicitar1", "Alocar")
-  // addItem("Solicitar(ajuste)", "solicitar2", "Alocar")
-  addItem("por Disciplina", "relatorio_por_disciplina", "Relatórios")
-  addItem("por Sala", "relatorio_por_sala", "Relatórios")
-  addItem("por Prédio", "relatorio_por_predio", "Relatórios")
-  menuDropdown("Usuários");
+  addItem("Salas", "salas", "Salas", "col1");
+  addItem("Categorias", "categorias", "Salas", "col1");
+  addItem("Relatório por Disciplina", "relatorio_por_disciplina", "Relatórios", "col2");
+  addItem("Relatório por Sala", "relatorio_por_sala", "Relatórios", "col2");
+  addItem("Relatório por Prédio", "relatorio_por_predio", "Relatórios", "col2");
 });
 
-function addItem(name, page, father) {
-  father = "#" + father
-  $(father).append("<li><a href='" + page + ".html'>" + name + "</a></li>")
+function addItem(name, page, father, colFooter) {
+  $("#" + father).append("<li><a href='" + page + ".html'>" + name + "</a></li>");
+  $("#" + colFooter).find(".myList").append("<li><a href='" + page + ".html'>" + name + "</a></li>");
 }
 
 function menuDropdown(name) {
-  $("#myNavBar").append("<li class='dropdown'><a data-toggle='collapse' href='#" + name + "'>" + name + "</a><ul class='collapse' id='" + name + "'></ul></li>")
+  $("#myNav").append('<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + name + '<span class="caret"></span></a><ul class="dropdown-menu" id="' + name + '"></ul></li>');
 }
