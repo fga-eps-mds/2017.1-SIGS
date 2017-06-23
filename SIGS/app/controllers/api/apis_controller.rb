dd69668b9a238861b36b34f427a70645455ae06b
Ateldy Brasil <ateldybfilho@gmail.com>
2017-06-23 11:37:00 -0300

    issue #84:  Implement 'Generate allocation information by building API'

--- SIGS/app/controllers/api/apis_controller.rb
+++ SIGS/app/controllers/api/apis_controller.rb
@@ -13,6 +13,34 @@ module Api
       render json: @rooms
     end
 
+    def buildings
+      building = Building.find(params[:id])
+      rooms = Room.where('building' => building.id)
+      allocation = Allocation.where('room' => rooms.ids)
+      @building_allocation = generate_return_building(building, allocation)
+      render json: @building_allocation
+    end
+
+    def generate_return_building(building, allocations)
+      hash = {}
+      allocations.each do |allocation|
+        hash[allocation.id] = {
+          building_name: building.name,
+          building_code: building.code, 
+          room_name: allocation.room.name,
+          room_capacity: allocation.room.capacity,
+          discipline_name: allocation.school_room.discipline,
+          discipline_code: allocation.school_room.discipline.code,
+          school_room_name: allocation.school_room.name,
+          school_room_vacancies: allocation.school_room.vacancies,
+          allocation_day: allocation.day,
+          allocation_start_time: allocation.start_time.strftime('%H:%M'),
+          allocation_final_time: allocation.final_time.strftime('%H:%M')
+        }
+      end
+      return hash
+    end
+
     private
 
     def authenticate?
