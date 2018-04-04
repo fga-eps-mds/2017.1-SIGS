bc47d8e06e35450e1e4530c74298b246e0eb51f2
MatheusRich <matheusrichardt@gmail.com>
2018-04-03 22:46:50 -0300

    Improve seeds

--- SIGS/db/seeds.rb
+++ SIGS/db/seeds.rb
@@ -7,17 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Departaments
+puts "Creating Departaments"
 department = Department.create(code: '789', name: 'Engenharia', wing: 'SUL')
 department_2 = Department.create(code: '781', name: 'PRC', wing: 'SUL')
 department_3 = Department.create(code: '156', name: 'Artes', wing: 'NORTE')
 
 # Courses
+puts "Creating Courses"
 course = Course.create(code: '10', name: 'Engenharia de Software', department: department, shift: 1)
 course_2 = Course.create(code: '12', name: 'Engenharia Eletrônica', department: department, shift: 1)
 course_3 = Course.create(code: '12', name: 'Engenharia Automotiva', department: department, shift: 2)
 course_4 = Course.create(code: '09', name: 'Artes Visuais', department: department_3, shift: 2)
 
 # Users - Coordinator
+puts "Creating Coordinator Users"
 user = User.create(name: 'Caio Filipe', email: 'caio@unb.br', cpf: '05012345678', registration: '1234567', active: 1, password: '123456')
 coordinator = Coordinator.create(user: user, course: course_2)
 user_2 = User.create(name: 'João Busche', email: 'joao@unb.br', cpf: '05044448888', registration: '1234544', active: 0, password: '123456')
@@ -26,18 +29,21 @@ user_3 = User.create(name: 'Daniel Marques', email: 'denes@unb.br', cpf: '050443
 coordinator_3 = Coordinator.create(user: user_3, course: course_4)
 
 # Users - Deg
+puts "Creating DEG Users"
 user_3 = User.create(name: 'João Pedro', email: 'pedro@unb.br', cpf: '05012349999', registration: '1234599', active: 1, password: '123456')
 department_assistant = Deg.create(user: user_3)
 user_4 = User.create(name: 'Ateldy Brasil', email: 'ateldy@unb.br', cpf: '05022446688', registration: '1234333', active: 0, password: '123456')
 department_assistant_2 = Deg.create(user: user_4)
 
 # Users - AdministrativeAssistant
+puts "Creating Administrative Assistant Users"
 user_5 = User.create(name: 'Wallacy Braz', email: 'wallacy@unb.br', cpf: '05012348888', registration: '1234588', active: 1, password: '123456')
 administrative_assistant = AdministrativeAssistant.create(user: user_5)
 user_6 = User.create(name: 'Carlos Aragon', email: 'carlos@unb.br', cpf: '05022248811', registration: '2224588', active: 0, password: '123456')
 administrative_assistant_2 = AdministrativeAssistant.create(user: user_6)
 
 # Buildings
+puts "Creating Buildings"
 buildings = Building.create([
   {code: 'pjc', name: 'Pavilhão João Calmon', wing: 'NORTE'},
   {code: 'PAT', name: 'Pavilhão Anísio Teixeira', wing: 'NORTE'},
@@ -46,10 +52,12 @@ buildings = Building.create([
   ])
 
 # Categories
+puts "Creating Categories"
 category = Category.create(name: 'Laboratório Químico')
 category_2 = Category.create(name: 'Retroprojetor')
 
 # Rooms
+puts "Creating Rooms"
 room = Room.create(code: '124325', name: 'S10', capacity: 50, active: true, time_grid_id: 1, department: department_3, building: buildings[1], category_ids: [category.id])
 room_2 = Room.create(code: '987653', name: 'SS', capacity: 40, active: false, time_grid_id: 2, department: department, building: buildings[2], category_ids: [category.id])
 room_3 = Room.create(code: '987655', name: 'S9', capacity: 40, active: false, time_grid_id: 2, department: department_2, building: buildings[2], category_ids: [category_2.id])
@@ -59,13 +67,14 @@ room_6 = Room.create(code: '987644', name: 'S2', capacity: 80, active: true, tim
 room_6 = Room.create(code: '987111', name: 'S4', capacity: 80, active: true, time_grid_id: 1, department: department_2, building: buildings[1], category_ids: [category.id])
 
 # Disciplines
+puts "Creating Disciplines"
 discipline = Discipline.create(code: '876', name: 'Cálculo 3', department: department)
 discipline_2 = Discipline.create(code: '777', name: 'Cálculo 2', department: department)
 discipline_3 = Discipline.create(code: '773', name: 'Cálculo 1', department: department)
 discipline_4 = Discipline.create(code: '774', name: 'Artes Visuais', department: department_3)
 
 #SchoolRooms
-
+puts "Creating School Rooms"
 school_room = SchoolRoom.create(name:'A', discipline: discipline, vacancies: 40, courses: [course_2])
 school_room_2 = SchoolRoom.create(name:'B', discipline: discipline, vacancies: 60, courses: [course_3])
 school_room_3 = SchoolRoom.create(name:'C', discipline: discipline_3, vacancies: 35, courses: [course_3])
@@ -74,11 +83,13 @@ school_room_5 = SchoolRoom.create(name:'AA', discipline: discipline_4, vacancies
 school_room_6 = SchoolRoom.create(name:'BB', discipline: discipline_4, vacancies: 40, course_ids: [course_4.id])
 
 # Periods
+puts "Creating Periods"
 period = Period.create(period_type:'Alocação', initial_date: '10-01-2018', final_date: '01-02-2018')
 period_2 = Period.create(period_type:'Ajuste', initial_date: '23-02-2018', final_date: '01-03-2018')
 period_3 = Period.create(period_type:'Letivo', initial_date: '08-03-2018', final_date: '14-07-2018')
 
 # Allocations
+puts "Creating Allocations"
 allocation = Allocation.create(user_id: user.id, room_id: room.id, school_room_id: school_room_2.id, day: "Segunda", start_time: '14:00:00', final_time: '16:00:00', active: true)
 allocation_2 = Allocation.create(user_id: user.id, room_id: room_2.id, school_room_id: school_room_2.id, day: "Quarta", start_time: '14:00:00', final_time: '16:00:00', active: true)
 allocation_3 = Allocation.create(user_id: user.id, room_id: room_2.id, school_room_id: school_room_2.id, day: "Sexta", start_time: '14:00:00', final_time: '16:00:00', active: true)
@@ -95,6 +106,7 @@ allocation_11 = Allocation.create(user_id: user.id, room_id: room_4.id, school_r
 allocation_12 = Allocation.create(user_id: user.id, room_id: room_4.id, school_room_id: school_room_6.id, day: "Quarta", start_time: '14:00:00', final_time: '18:00:00', active: true)
 
 # Extension Allocations
+puts "Creating Extension Allocations"
 allocation_extension_1 = AllocationExtension.create(user_id: user.id, room_id: room.id, start_time: '18:00:00', final_time: '19:00:00', inicial_date: '06-07-2017', final_date: '06-07-2017', periodicity: "Quinzenal")
 
 # Token and Secrets
@@ -106,6 +118,7 @@ SECRET_3 ||= '$2a$10$OzJkJBid71H2SRlVxv71buTP2hHoDFe4dIqIV9QjWebYy222W4Uoa'
 TOKEN_3 ||= 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiTWF0aGV1cyBGcmFuY2lzY28gZG9zIFNhbnRvcyIsImVtYWlsIjoibWF0aGV1cy1mcmFuY2lzY29AeWFob28uY29tIn0.-z3L4M7C5-7JYNeKv_UrKaRhEVtdtsP5ho3w66Xl6PM'
 
 # API Users
+puts "Creating API Users"
 api_user = ApiUser.create(name: 'Ana Paula Chaves', email: 'anapaula.chaves@gmail.com', secret: SECRET, token: TOKEN, user: user_5)
 api_user_2 = ApiUser.create(name: 'Gustavo Freire Oliveira', email: 'freire.oliveira@hotmail.com', secret: SECRET_2, token: TOKEN_2, user: user_5)
 api_user_3 = ApiUser.create(name: 'Matheus Francisco dos Santos', email: 'matheus-francisco@yahoo.com', secret: SECRET_3, token: TOKEN_3, user: user)
